015647447cc8ec3eb0da09c8e783bc3bc29677b6
william <williampius17@gmail.com>
2021-01-28 00:31:46 +0300

    added search textinput in home

--- app/screens/Home/index.tsx
+++ app/screens/Home/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View } from 'react-native';
-import { Button, List } from 'react-native-paper';
+import { Button, List, TextInput } from 'react-native-paper';
 import { Image, ScrollView, Text } from 'react-native';
 import { useDispatch } from 'react-redux';
 import * as loginActions from 'app/store/actions/loginActions';
@@ -16,43 +16,14 @@ const Home: React.FC = () => {
   };
   return (
     <View style={styles.container}>
+      <TextInput
+        mode="outlined"
+        placeholder="Search Art"
+        style={styles.search}
+      />
       <Button icon="logout" mode="contained" onPress={onLogout}>
         Logout
       </Button>
-     
-      <ScrollView>
-        <Text style={{ fontSize: 96 }}>Scroll me plz</Text>
-        <Image source={logo} />
-        <Image source={logo} />
-        <Image source={logo} />
-        <Image source={logo} />
-        <Image source={logo} />
-        <Text style={{ fontSize: 96 }}>If you like</Text>
-        <Image source={logo} />
-        <Image source={logo} />
-        <Image source={logo} />
-        <Image source={logo} />
-        <Image source={logo} />
-        <Text style={{ fontSize: 96 }}>Scrolling down</Text>
-        <Image source={logo} />
-        <Image source={logo} />
-        <Image source={logo} />
-        <Image source={logo} />
-        <Image source={logo} />
-        <Text style={{ fontSize: 96 }}>What's the best</Text>
-        <Image source={logo} />
-        <Image source={logo} />
-        <Image source={logo} />
-        <Image source={logo} />
-        <Image source={logo} />
-        <Text style={{ fontSize: 96 }}>Framework around?</Text>
-        <Image source={logo} />
-        <Image source={logo} />
-        <Image source={logo} />
-        <Image source={logo} />
-        <Image source={logo} />
-        <Text style={{ fontSize: 80 }}>React Native</Text>
-      </ScrollView>
     </View>
   );
 };
--- app/screens/Home/styles.ts
+++ app/screens/Home/styles.ts
@@ -3,9 +3,15 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
   },
+  search:{
+    width:290,
+    borderRadius:30,
+    margin:10,
+    height:40
+  }
 });
 
 export default styles;
