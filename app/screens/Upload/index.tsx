51b010f18813c83790ee604013fdbbbd56637cab
gachagajnr <williampius17@users.noreply.github.com>
2021-04-22 09:15:11 +0300

    can choose image

--- app/screens/Upload/index.tsx
+++ app/screens/Upload/index.tsx
@@ -1,8 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView, ScrollView, Text } from 'react-native';
+import * as ImagePicker from 'react-native-image-picker';
+import {
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Button,
+  Image,
+} from 'react-native';
+
 import styles from './styles';
 
 const Upload: React.FC = () => {
+  const [response, setResponse] = useState(null);
   useEffect(() => {
     // setArt(props.route.params.item);
   }, []);
@@ -11,6 +21,54 @@ const Upload: React.FC = () => {
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.scrollView}>
         <Text>Upload Art</Text>
+        <Button
+          title="Take image"
+          onPress={() =>
+            ImagePicker.launchCamera(
+              {
+                mediaType: 'photo',
+                includeBase64: false,
+                maxHeight: 200,
+                maxWidth: 200,
+              },
+              (response) => {
+                setResponse(response);
+              },
+            )
+          }
+        />
+        <Button
+          title="Select image"
+          onPress={() =>
+            ImagePicker.launchImageLibrary(
+              {
+                mediaType: 'photo',
+                includeBase64: false,
+                maxHeight: 200,
+                maxWidth: 200,
+              },
+              (response) => {
+                setResponse(response);
+              },
+            )
+          }
+        />
+
+        <View style={styles.response}>
+          <Text>Res: {JSON.stringify(response)}</Text>
+        </View>
+
+        {response && (
+          <View style={styles.image}>
+            <Image
+              style={{ width: 200, height: 200 }}
+              source={{ uri: response.uri }}
+            />
+          </View>
+        )}
+      
+
+        
       </ScrollView>
     </SafeAreaView>
   );
--- app/screens/Upload/styles.ts
+++ app/screens/Upload/styles.ts
@@ -4,7 +4,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  
+
   scrollView: {
     backgroundColor: 'white',
     padding: 10,
@@ -19,6 +19,18 @@ const styles = StyleSheet.create({
     height: 400,
     padding: 5,
     resizeMode: 'contain',
+  },
+  button: {
+    marginVertical: 24,
+    marginHorizontal: 24,
+  },
+  image: {
+    marginVertical: 24,
+    alignItems: 'center',
+  },
+  response: {
+    marginVertical: 16,
+    marginHorizontal: 8,
   },
 });
 
